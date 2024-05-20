180ec2cca9b93e87e320f4fe6ecdd6fd937681cd
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2024-05-20 20:08:50 -0300

    init project

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.transform.gerar_dataframe import DataFrameTransformer
+from src.extract.crawler import CrawlerCompassFT
+
+crawler = CrawlerCompassFT()
+dados = crawler.run()
+
+
+transformer = DataFrameTransformer()
+colunas = ["Ticker", "Valor", "Data"]
+df = transformer.create_dataframe(dados, colunas)
+df = transformer.format_data_dataframe(df)
+top_df = transformer.select_top_n_rows(df, 10)
+print(top_df)
